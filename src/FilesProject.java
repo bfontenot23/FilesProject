6a0489b345c0cb69a76d643b37e424ce7f7cda87
Beau Fontenot <104475813+bfontenot23@users.noreply.github.com>
2024-01-26 13:53:31 -0600

    Complete lab to specifications

--- src/FilesProject.java
+++ src/FilesProject.java
@@ -1,8 +1,13 @@
+//Student Name: Beau A Fontenot
+//LSU ID: 896506087
+//Lab Section: 002 (1:30pm F)
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Random;
+import java.util.Scanner;
 
 public class FilesProject {
 
@@ -26,6 +31,21 @@ public class FilesProject {
             output.println(rand.nextInt(0, 1001));
         }
         output.close();
+
+        File inputFile = new File("./src/calculations.txt");
+        int[] nums = new int[1000];
+        Scanner input = null;
+        try { input = new Scanner(inputFile); }
+        catch(FileNotFoundException e) { System.out.println(e); }
+
+        for(int i = 0; i<1000; i++) { nums[i] = input.nextInt(); }
+
+        int sum = 0;
+        for (int num : nums) { sum += num; }
+
+        double average = (double) sum /nums.length;
+
+        System.out.printf("\nThe average of all the numbers in calculations.txt is %.2f.", average);
 
     }
 }
